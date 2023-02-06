c862f60bbb7eb2e929e8999ccac8d45569b79784
Your Name <you@example.com>
2023-02-05 21:10:17 -0500

    found carmen

--- answers.sql
+++ answers.sql
@@ -79,14 +79,15 @@ SELECT * FROM Country
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
-SELECT * FROM CITY
-    INNER JOIN COUNTRY
-        ON country.capital = city.id
+SELECT * FROM Country
+    INNER JOIN city
+        ON city.id = country.capital
             where country.code = 'BRA';
 
 
 -- countrycode - "BRA"
 -- country name - Brazil
+-- city name - Brasï¿½lia
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
@@ -100,10 +101,11 @@ SELECT * FROM CITY
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
 
+SELECT * FROM city where population=91084;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
 
 
--- She's in ____________________________!
+-- She's in _Santa Monica California___________________________!
